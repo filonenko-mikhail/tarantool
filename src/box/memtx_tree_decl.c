23eb1b1c06c556725e8803ebdf17209c9af23ca5
Alexandr Lyapunov <aleks@tarantool.org>
2017-10-16 17:04:41 +0300

    memtx: introduce tuple compare hint
    Implement functions for retrieving tuple hints for a particular
    key_def. Hint is an integer that can be used for tuple comparison
    optimization: if a hint of one tuple is less than a hint of another
    then the first tuple is definitely less than the second; only if
    hints are equal tuple_compare must be called for getting comparison
    result.
    
    Hints are calculated using only the first part of key_def.
    
    Hints are only useful when:
     * they are precalculated and stored along with the tuple;
    calculation is not cheap (cheaper than tuple_compare btw) but
    precalculated hints allow to compare tuples without even fetching
    tuple data.
     * first part of key_def is 'string'(for now)
     * since hint is calculated using only the first part of key_def
    (and only first several characters if it is a string) this part
    must be different with high probability for every tuple pair.
    
    Closes #3961

--- src/box/memtx_tree_decl.c
+++ src/box/memtx_tree_decl.c
@@ -77,8 +77,97 @@ struct memtx_basic_tree_key_data {
 
 /* }}} */
 
+/* {{{ Memtx hinted tree class. *********************************/
+
+/**
+ * Struct that is used as a key in BPS tree definition in
+ * memtx_hint_only_tree and memtx_hinted_tree.
+*/
+struct memtx_hinted_tree_key_data {
+	/** Sequence of msgpacked search fields. */
+	const char *key;
+	/** Number of msgpacked search fields. */
+	uint32_t part_count;
+	/**
+	 * Comparison hint. Calculated automatically on 'set'
+	 * operation with MEMTX_TREE_KEY_SET().
+	 */
+	uint64_t hint;
+};
+
+/**
+ * Struct that is used as a key in BPS tree definition in
+ * memtx_hint_only_tree and memtx_hinted_tree.
+ */
+struct memtx_hinted_tree_data {
+	/** Tuple this node is represents. */
+	struct tuple *tuple;
+	/**
+	 * Comparison hint. Calculated automatically on 'set'
+	 * operation with MEMTX_TREE_ELEM_SET().
+	 */
+	uint64_t hint;
+};
+
+#define MEMTX_TREE_NAME memtx_hinted_tree
+#define memtx_tree_elem_t struct memtx_hinted_tree_data
+#define memtx_tree_key_t struct memtx_hinted_tree_key_data
+#define MEMTX_TREE_COMPARE(elem_a_ptr, elem_b_ptr, key_def) ({			\
+	int rc;									\
+	if ((elem_a_ptr)->hint != (elem_b_ptr)->hint) {				\
+		rc = (elem_a_ptr)->hint < (elem_b_ptr)->hint ? -1 : 1;		\
+	} else {								\
+		rc = tuple_compare((elem_a_ptr)->tuple, (elem_b_ptr)->tuple,	\
+				   key_def);					\
+	}									\
+	rc;									\
+})
+#define MEMTX_TREE_COMPARE_KEY(elem_ptr, key_ptr, key_def) ({			\
+	int rc;									\
+	if ((elem_ptr)->hint != (key_ptr)->hint) {				\
+		rc = (elem_ptr)->hint < (key_ptr)->hint ? -1 : 1;		\
+	} else {								\
+		rc = tuple_compare_with_key((elem_ptr)->tuple, (key_ptr)->key,	\
+					    (key_ptr)->part_count, key_def);	\
+	}									\
+	rc;									\
+})
+#define MEMTX_TREE_ELEM_SET(elem_ptr, info, key_def) ({				\
+	(elem_ptr)->tuple = info;						\
+	(elem_ptr)->hint = tuple_hint(info, key_def);				\
+})
+#define MEMTX_TREE_KEY_SET(key_ptr, key_val, part_count_val, key_def) ({	\
+	(key_ptr)->key = key_val;						\
+	(key_ptr)->part_count = part_count_val;					\
+	(key_ptr)->hint = part_count_val > 0 ? key_hint(key_val, key_def) : 0;	\
+})
+#define MEMTX_TREE_ELEM_GET(elem_ptr) ((elem_ptr)->tuple)
+#define MEMTX_TREE_KEY_GET(key_ptr, part_count_ptr)				\
+	({*part_count_ptr = (key_ptr)->part_count; (key_ptr)->key;})
+#define MEMTX_TREE_IDENTICAL(elem_a_ptr, elem_b_ptr)				\
+	({(elem_a_ptr)->tuple == (elem_b_ptr)->tuple;})
+
+#include "memtx_tree_impl.h"
+
+#undef memtx_tree_key_t
+#undef memtx_tree_elem_t
+#undef MEMTX_TREE_KEY_GET
+#undef MEMTX_TREE_ELEM_GET
+#undef MEMTX_TREE_KEY_SET
+#undef MEMTX_TREE_ELEM_SET
+#undef MEMTX_TREE_COMPARE_KEY
+#undef MEMTX_TREE_COMPARE
+#undef MEMTX_TREE_NAME
+#undef MEMTX_TREE_IDENTICAL
+
+/* }}} */
+
 struct index *
 memtx_tree_index_new(struct memtx_engine *memtx, struct index_def *def)
 {
+	if (def->cmp_def->parts->type == FIELD_TYPE_STRING ||
+	    def->cmp_def->parts->type == FIELD_TYPE_UNSIGNED ||
+	    def->cmp_def->parts->type == FIELD_TYPE_INTEGER)
+		return memtx_hinted_tree_index_new(memtx, def);
 	return memtx_basic_tree_index_new(memtx, def);
 }
