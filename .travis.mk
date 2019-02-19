84ea4dbff8f95eba4180656c01e6bba15554b35e
Alexander V. Tikhonov <avtikhon@tarantool.org>
2019-02-19 09:10:12 +0300

    travis-ci: fixed Mojave mac build with CMAKE_OSX_DEPLOYMENT_TARGET
    Specify the minimum version of OS X on which the target
    binaries are to be deployed. CMake uses this value for the
    -mmacosx-version-min flag and to help choose the default SDK

--- .travis.mk
+++ .travis.mk
@@ -46,10 +46,10 @@ test_ubuntu: deps_ubuntu
 
 deps_osx:
 	brew update
-	brew install openssl readline curl icu4c pyenv-virtualenv --force
-	curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | python
+	brew install openssl readline curl icu4c --force
+	pip -h >/dev/null 2>&1 || curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | python
 	pip --version
-	pip install virtualenv
+	virtualenv -h >/dev/null 2>&1 || ( brew install pyenv-virtualenv --force ; pip install virtualenv )
 
 test_osx: deps_osx
 	cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfoWError ${CMAKE_EXTRA_PARAMS}
